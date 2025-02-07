a35d793d7d165dc156d5ee62a82c03f5e102b637
JosueSay <106031855+JosueSay@users.noreply.github.com>
2025-02-06 18:56:10 -0600

    feat: nueva conexion db

---
+++ labs/lab3/conn.py
@@ -0,0 +1,10 @@
+from neo4j import GraphDatabase
+
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = "neo4j+s://d75b418a.databases.neo4j.io"
+AUTH = ("neo4j", "jQAeKwuVMjIOOtDwpRjfs4RjfrMHXdPhzUzKmJramRw")
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+    print("Connection established.")
+
