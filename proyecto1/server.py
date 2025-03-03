b2854f336e2d2302144afbfd4479435ffe075caa
abbydoag <abby.donis.a@gmail.com>
2025-03-03 08:39:27 -0600

    Endpoints

--- proyecto1/server.py
+++ proyecto1/server.py
@@ -1,5 +1,16 @@
-from flask import Flask
+<<<<<<< HEAD
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
+from conn import connection
+from baseMethods import clean_db, create_nodo, create_relation, transaction_record
+=======
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+from conn import connection
+from baseMethods import *
+from flask_cors import CORS
+
+>>>>>>> e665323c5e0ea49cab4cb1b5fb1489c10fe63986
 import os
 
 # Cargar variables de entorno desde .env
@@ -10,6 +21,9 @@ URI = "neo4j+s://3b37977c.databases.neo4j.io"
 AUTH = ("neo4j", "W_m4ObG8GYoBCCcUVjQTBJlGj44enIFNCPVgvR8GEb4")
 
 app = Flask(__name__)
+driver = connection(URI,AUTH)
+
+CORS(app, origins=["http://localhost:3000"])
 
 # Configuración de Flask con variables de entorno
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
@@ -18,6 +32,107 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 @app.route('/')
 def home():
     return f"Flask está corriendo en modo: {os.getenv('FLASK_ENV')}"
+
+<<<<<<< HEAD
+@app.route('/clean_db', methods=['DELETE'])
+def clean_database():
+    clean_db(driver)
+    return jsonify ({"Se ha limpiado la base de datos."})
+
+@app.route('/create_nodo', methods=['POST'])
+def creating_node():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No se han encontrado datos"}), 400
+    create_nodo(driver, data)
+    return jsonify({"message": "Nodo creado exitosamente"})
+
+@app.route('/create_relationship', methods=['POST'])
+def creating_relationship():
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No se han encontrado datos'}), 400
+    create_relation(driver, data)
+    return jsonify({'message': 'Relacion creada exitosamente'})
+
+@app.route('/transaction_log', method=['POST'])
+def transaction_registration():
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No se han encontrado datos'}), 400
+    transaction_record(driver, data)
+    return jsonify({'message':'Transaccion registrada'})
+=======
+
+
+@app.route('/get_customers')
+def customers():
+    driver = connection()
+    nodos = get_all(driver, 'Customer', limitation=200)
+    
+    # Convertir cada objeto a diccionario
+    nodos_dict = [n.ge_propiedades_dic() for n in nodos]
+
+    driver.close()
+    
+    # Retornar JSON con los nodos
+    return jsonify(nodos_dict)
+
+
+@app.route('/get_devices')
+def devices():
+    driver = connection()
+    nodos = get_all(driver, 'Device', limitation=10)
+    
+    # Convertir cada objeto a diccionario
+    nodos_dict = [n.ge_propiedades_dic() for n in nodos]
+
+    driver.close()
+    
+    # Retornar JSON con los nodos
+    return jsonify(nodos_dict)
+
+@app.route('/get_accounts')
+def accounts():
+    driver = connection()
+    nodos = get_all(driver, 'Bank_Account', limitation=10)
+    
+    # Convertir cada objeto a diccionario
+    nodos_dict = [n.ge_propiedades_dic() for n in nodos]
+
+    driver.close()
+    
+    # Retornar JSON con los nodos
+    return jsonify(nodos_dict)
+
+@app.route('/get_comerciantes')
+def merchants():
+    driver = connection()
+    nodos = get_all(driver, 'Merchant', limitation=2000)
+    
+    # Convertir cada objeto a diccionario
+    nodos_dict = [n.ge_propiedades_dic() for n in nodos]
+
+    driver.close()
+    
+    # Retornar JSON con los nodos
+    return jsonify(nodos_dict)
+
+
+@app.route('/get_transaction')
+def transacciones():
+    driver = connection()
+    nodos = get_all(driver, 'Transaction', limitation=2000)
+    
+    # Convertir cada objeto a diccionario
+    nodos_dict = [n.ge_propiedades_dic() for n in nodos]
+
+    driver.close()
+    
+    # Retornar JSON con los nodos
+    return jsonify(nodos_dict)
+
+>>>>>>> e665323c5e0ea49cab4cb1b5fb1489c10fe63986
 
 if __name__ == '__main__':
     app.run(debug=True)
